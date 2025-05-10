a6feed812f9c5bba3076131d619560aab989e784
RickRick84 <flsheandoxahi@gmail.com>
2025-05-10 16:21:03 -0500

    🔥 Archivos definitivos para deploy funcional sin pantalla blanca

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.', // raíz del proyecto donde está index.html
   base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
