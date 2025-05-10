629ba08c38f423f99cc24252bb33c34fce9cebe0
RickRick84 <flsheandoxahi@gmail.com>
2025-05-10 16:41:30 -0500

    📦 Fix entrada main.jsx import simplificado

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 
 const root = document.getElementById('root');
 
