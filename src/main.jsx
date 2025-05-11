80421cd2894d2d461fb98511b84793fe9036afda
RickRick84 <flsheandoxahi@gmail.com>
2025-05-11 13:11:55 -0500

    cambios App.css y Main.jsx para arreglar Mobile

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
-import './MobileFix.css';
 import './components/SidebarMenu.css';
 import './components/UserMenu.css';
 
